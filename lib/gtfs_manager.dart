1a314667f8f7fe4ad942d0444f373fe0cb80e0a5
Andres <rb.andres98@gmail.com>
2019-09-06 00:08:46 -0500

    Set Exports

--- lib/gtfs_manager.dart
+++ lib/gtfs_manager.dart
@@ -5,5 +5,8 @@ library gtfs_manager;
 
 export 'src/gtfs_manager_base.dart';
 export 'src/agency.dart';
+export 'src/routes.dart';
+export 'src/calendar.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
